d9c12dec3f18e7bb6743b6e392dda39741509353
Your Name <jaceman44@gmail.com>
2022-05-31 23:42:04 +0200

    Added simple RGB tresholdinig

--- treshholding/tresholding.py
+++ treshholding/tresholding.py
@@ -3,29 +3,28 @@
 import cv2 as cv
 import os
 
+
 def simple_tresholding():
-    CONFIG_DIR = os.path.dirname(__file__) + "/example_picture/"
-    
-    
-    img = cv.imread(CONFIG_DIR+'22678960_15.png',0)
+    # CONFIG_DIR = os.path.dirname(__file__) + "/example_picture/"
 
-    _, th1 = cv.threshold(img, 150, 255, cv.THRESH_BINARY)
+    # img = cv.imread(CONFIG_DIR+'22678960_15.png', 0)
 
-    # _, th2 = cv.threshold(img, 200, 255, cv.THRESH_BINARY_INV)
+    # _, th1 = cv.threshold(img, 150, 255, cv.THRESH_BINARY)
 
-    #_, th3 = cv.threshold(img, 100, 255, cv.THRESH_TRUNC)
+    # # _, th2 = cv.threshold(img, 200, 255, cv.THRESH_BINARY_INV)
 
-    # _, th4 = cv.threshold(img, 100, 255, cv.THRESH_TOZERO)
+    # #_, th3 = cv.threshold(img, 100, 255, cv.THRESH_TRUNC)
 
-    # _, th5 = cv.threshold(img, 100, 255, cv.THRESH_TOZERO_INV)
+    # # _, th4 = cv.threshold(img, 100, 255, cv.THRESH_TOZERO)
 
-    # th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-    # th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 5, 2)
+    # # _, th5 = cv.threshold(img, 100, 255, cv.THRESH_TOZERO_INV)
 
+    # # th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+    # # th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 5, 2)
 
-    cv.imshow("Image", img)
+    # cv.imshow("Image", img)
 
-    cv.imshow("th2",th1)
+    # cv.imshow("th2", th1)
     # cv.imshow("th3",th3)
 
     # cv.imshow("th1", th1)
@@ -33,6 +32,34 @@ def simple_tresholding():
     # cv.imshow("th3", th3)
     # cv.imshow("th4", th4)
     # cv.imshow("th5", th5)
+
+    window_detection_name = 'Simple thresholding'
+
+    cv.namedWindow(window_detection_name)
+
+
+    #while True:
+
+    CONFIG_DIR = os.path.dirname(__file__) + "/example_picture/"
+
+    frame = cv.imread(CONFIG_DIR+'22678915_15.png', 3)
+
+    frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    frame_threshold = cv.inRange(frame_HSV, (101, 0, 0), (115, 84, 255))
+
+
+    frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+
+    frame_threshold = cv.inRange(frame_HSV, (101, 0, 0), (115, 84, 255))
+
+    width = 720
+    height = 720
+    dim = (width, height)
+
+    resized = cv.resize(frame_threshold, dim, interpolation=cv.INTER_AREA)
+
+    cv.imshow(window_detection_name, resized)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
